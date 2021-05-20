de10545d101da891184619fc27c1a926f034b8a8
Lucyna <lucyna.klimiuk@o2.pl>
2021-05-20 19:52:28 +0200

    PierwszeEkrany

--- app/src/main/java/com/example/czystezbki/MainActivity.kt
+++ app/src/main/java/com/example/czystezbki/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.czystezbki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var dalej: Button? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dalej=findViewById(R.id.ButtonNextView)
+
+        dalej?.setOnClickListener(object : View.OnClickListener{
+                override fun onClick(v: View?){
+                    akt_WprDanych()
+                }
+        })
+
     }
+    private fun akt_WprDanych(){
+        val intent= Intent(this,WprowadzanieDanych::class.java)
+        startActivity(intent)
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/czystezbki/WprowadzanieDanych.kt
@@ -0,0 +1,11 @@
+package com.example.czystezbki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WprowadzanieDanych : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wprowadzanie_danych)
+    }
+}
