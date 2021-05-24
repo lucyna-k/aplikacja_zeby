cfc7af24a6d2c42fa527449117df31207f4c7990
Lucyna <lucyna.klimiuk@o2.pl>
2021-05-24 15:49:36 +0200

    TrzecieEkrany_pop

--- app/src/main/java/com/example/czystezbki/WyborOpcji.kt
+++ app/src/main/java/com/example/czystezbki/WyborOpcji.kt
@@ -1,11 +1,47 @@
 package com.example.czystezbki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+private var termin: Button?=null
+private var mycie: Button?=null
+
+
 
 class WyborOpcji : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wybor_opcji)
+
+        termin=findViewById(R.id.btntermin)
+        mycie=findViewById(R.id.btnMycie)
+
+        termin?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?){
+                akt_SprTermin()
+            }
+        })
+
+        mycie?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?){
+                akt_Mycie()
+            }
+        })
+
     }
-}
+    private fun akt_SprTermin(){
+        val intent= Intent(this,TerminWizyty::class.java)
+        startActivity(intent)
+
+    }
+    private fun akt_Mycie(){
+        val intent= Intent(this,ZaczynamyMycie::class.java)
+        startActivity(intent)
+
+    }
+
+
+    }
--- app/src/main/java/com/example/czystezbki/ZaczynamyMycie.kt
+++ app/src/main/java/com/example/czystezbki/ZaczynamyMycie.kt
@@ -1,11 +1,33 @@
 package com.example.czystezbki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+private var start: Button?=null
 
 class ZaczynamyMycie : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zaczynamy_mycie)
+
+
+        start = findViewById(R.id.btnStart)
+
+        start?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                akt_Mycie1()
+            }
+        })
+
+
+    }
+
+    private fun akt_Mycie1() {
+        val intent = Intent(this, Mycie1::class.java)
+        startActivity(intent)
+
     }
 }
